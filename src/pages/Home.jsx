21be9d297a99e22c9d16785a8beceb043fa89e1a
Ronalds <ronalds.borisovs@inbox.lv>
2024-02-04 22:47:14 +0200

    c

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,84 +43,113 @@ export const Home = () => {
         <Tab label="Новые" onClick={changeActiveNew} />
         <Tab label="Популярные" onClick={changeActivePopular} />
       </Tabs>
-      <Grid container spacing={4}>
-        <Grid xs={8} item>
-          {(isPostsLoading
-            ? [...Array(5)]
-            : posts.items
-                .slice()
-                .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-          ).map((obj, index) =>
-            isPostsLoading ? (
-              <Post isLoading={true} key={index} />
-            ) : (
-              <Post
-                _id={obj._id}
-                title={obj.title}
-                imageUrl={
-                  obj.imageUrl
-                    ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
-                    : ''
-                }
-                user={obj.user}
-                createdAt={obj.createdAt}
-                viewsCount={obj.viewsCount}
-                commentsCount={3}
-                tags={obj.tags}
-                isEditable={userData?._id === obj.user._id} //prava na redaktirovanije statej
-              />
-            )
-          )}
-        </Grid>
-        <Grid xs={8} item>
-          {(isPostsLoading
-            ? [...Array(5)]
-            : posts.items.slice().sort((a, b) => b.viewsCount - a.viewsCount)
-          ).map((obj, index) =>
-            isPostsLoading ? (
-              <Post isLoading={true} key={index} />
-            ) : (
-              <Post
-                _id={obj._id}
-                title={obj.title}
-                imageUrl={
-                  obj.imageUrl
-                    ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
-                    : ''
-                }
-                user={obj.user}
-                createdAt={obj.createdAt}
-                viewsCount={obj.viewsCount}
-                commentsCount={3}
-                tags={obj.tags}
-                isEditable={userData?._id === obj.user._id} //prava na redaktirovanije statej
-              />
-            )
-          )}
-        </Grid>
-        <Grid xs={4} item>
-          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
-          <CommentsBlock
-            items={[
-              {
-                user: {
-                  fullName: 'Вася Пупкин',
-                  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+
+      {isActive === 0 ? (
+        <Grid container spacing={4}>
+          <Grid xs={8} item>
+            {(isPostsLoading
+              ? [...Array(5)]
+              : posts.items
+                  .slice()
+                  .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+            ).map((obj, index) =>
+              isPostsLoading ? (
+                <Post isLoading={true} key={index} />
+              ) : (
+                <Post
+                  _id={obj._id}
+                  title={obj.title}
+                  imageUrl={
+                    obj.imageUrl
+                      ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
+                      : ''
+                  }
+                  user={obj.user}
+                  createdAt={obj.createdAt}
+                  viewsCount={obj.viewsCount}
+                  commentsCount={3}
+                  tags={obj.tags}
+                  isEditable={userData?._id === obj.user._id} //prava na redaktirovanije statej
+                />
+              )
+            )}
+          </Grid>
+
+          <Grid xs={4} item>
+            <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+            <CommentsBlock
+              items={[
+                {
+                  user: {
+                    fullName: 'Вася Пупкин',
+                    avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+                  },
+                  text: 'Это тестовый комментарий',
                 },
-                text: 'Это тестовый комментарий',
-              },
-              {
-                user: {
-                  fullName: 'Иван Иванов',
-                  avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                {
+                  user: {
+                    fullName: 'Иван Иванов',
+                    avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                  },
+                  text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
                 },
-                text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
-              },
-            ]}
-            isLoading={false}
-          />
+              ]}
+              isLoading={false}
+            />
+          </Grid>
         </Grid>
-      </Grid>
+      ) : (
+        <Grid container spacing={4}>
+          <Grid xs={8} item>
+            {(isPostsLoading
+              ? [...Array(5)]
+              : posts.items.slice().sort((a, b) => b.viewsCount - a.viewsCount)
+            ).map((obj, index) =>
+              isPostsLoading ? (
+                <Post isLoading={true} key={index} />
+              ) : (
+                <Post
+                  _id={obj._id}
+                  title={obj.title}
+                  imageUrl={
+                    obj.imageUrl
+                      ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
+                      : ''
+                  }
+                  user={obj.user}
+                  createdAt={obj.createdAt}
+                  viewsCount={obj.viewsCount}
+                  commentsCount={3}
+                  tags={obj.tags}
+                  isEditable={userData?._id === obj.user._id} //prava na redaktirovanije statej
+                />
+              )
+            )}
+          </Grid>
+          <Grid xs={4} item>
+            <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+            <CommentsBlock
+              items={[
+                {
+                  user: {
+                    fullName: 'Вася Пупкин',
+                    avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+                  },
+                  text: 'Это тестовый комментарий',
+                },
+                {
+                  user: {
+                    fullName: 'Иван Иванов',
+                    avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                  },
+                  text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
+                },
+              ]}
+              isLoading={false}
+            />
+          </Grid>
+        </Grid>
+      )}
     </>
   )
 }
